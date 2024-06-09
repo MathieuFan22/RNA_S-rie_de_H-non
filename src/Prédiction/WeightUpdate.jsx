66dfd2dce96f8ddc9ad8e9998459c7faf6f2ccbc
MathieuFan22 <fanirintsoaandrianandraina@gmail.com>
2024-06-09 15:32:15 +0300

    Delta de la couche de sortie

--- src/Prédiction/WeightUpdate.jsx
+++ src/Prédiction/WeightUpdate.jsx
@@ -2,66 +2,100 @@ import '../App.css';
 // import calculateXY from './get500Values';
 
 function WeightUpdate() {
-    const prototype = [1, 0, 1]
-    const step = 0.1
-    const desiredOutput = [1]
+    const prototype = [0, 1, 1];
+    const step = 0.1;
+    const desiredOutput = [1, 0.5, 0.3];
     const w = [
         [
+            [0.1, 0.1],
             [0.2, 0.3],
-            [0.1, 0.2],
-            [0.1, 0.3]
+            [0.1, 0.2]
         ],
         [
-            [0.2],
-            [0.3]
+            [0.2, 0.2, 0.2],
+            [0.1, 0.3, 0.1]
         ]
-    ]
+    ];
     
-    const V = [[...prototype], []]
-    const delta = []
-
-    const sigmoideFunction = (x) => {
-        return 1 / (1 + Math.exp(-x))
-    }
-    const sigmoideFunctionDerivated = (x) => {
-        return Math.exp(-x) / (1 + Math.exp(-x))**2
-    }
+    const V = [[...prototype], [], []];
+    const delta = [[], []];
+    
+    const sigmoid = (x) => 1 / (1 + Math.exp(-x));
+    
+    const sigmoidDerivative = (x) => Math.exp(-x) / ((1 + Math.exp(-x)) ** 2);
+    
+    const calculateActivation = (weights, inputs) => {
+        let sum = weights.reduce((sum, weight, index) => sum + weight * inputs[index], 0);
+        return sigmoid(sum);
+    };
+    
     const forwardPropagation = () => {
-        let tmp = []
-        let s = 0
+        console.log("Begin: Vers l'avant");
         for (let m = 0; m < 2; m++) {
-            console.log("Pour m=" + parseInt(m+2))
+            console.log(`Pour m=${m + 2}`);
             for (let i = 0; i < w[m][0].length; i++) {
-                console.log("\tPour i= " + parseInt(i+1))
-                for (let j = 0; j < w[m].length; j++) {
-                   tmp.push(w[m][j][i]);
-                }
-                for (let index = 0; index < tmp.length; index++) {
-                    tmp[index] = tmp[index] * V[m][index] 
-                }
-                s = tmp.reduce((a, b)=> {
-                    return a+b
-                });
-                V[1].push(sigmoideFunction(s))
-                console.log(sigmoideFunction(s))
-                tmp.length = 0
+                // Au lieu de faire un autre boucle for comme dans le leçon (sur m, i et j), on extracte w[m][j][i] et le stocke dans weight
+                const weights = w[m].map(row => row[i]);
+                const activation = calculateActivation(weights, V[m]);
+                V[m + 1].push(activation);
+                console.log(activation);
             }
-            console.log('\n\n')
+            console.log('\n\n');
         }
-        console.log(V)
-    }
-
-    const outputLayerDelta = () => {
-        let tmp = []
-        for (let m = 0; m < w[1][0].length; m++) {
-            
-            
+        console.log("End: Vers l'avant");
+        calculateOutputLayerDelta();
+    };
+    
+    const calculateOutputLayerDelta = () => {
+        console.log("Begin: Delta sortie");
+        for (let i = 0; i < w[1][0].length; i++) {
+            // Au lieu de faire un autre boucle for comme dans le leçon (sur m, i et j), on extracte w[m][j][i] et le stocke dans weight
+            const weights = w[1].map(row => row[i]);
+            let sum = weights.reduce((acc, weight, index) => acc + weight * V[1][index], 0);
+            let deltaValue = sigmoidDerivative(sum) * (desiredOutput[i] - V[2][i]);
+            delta[0].push(deltaValue);
         }
-    }
+        console.log(delta[0]);
+        console.log("End: Delta sortie");
+    };
+    
+  
+   
+    
+    
+    const hiddenLayerDelta = () => {
+        let tmp = [];
+        for (let i = 0; i < w[0].length; i++) {
+            let error = 0;
+            for (let j = 0; j < w[1].length; j++) {
+                error += delta[0][j] * w[1][j][i];
+            }
+            tmp[i] = error * sigmoideFunctionDerivated(V[0][i]);
+        }
+        delta.unshift(tmp);
+    };
 
+    const updateWeights = () => {
+        for (let m = 0; m < w.length; m++) {
+            for (let i = 0; i < w[m].length; i++) {
+                for (let j = 0; j < w[m][i].length; j++) {
+                    w[m][i][j] += step * delta[m][i] * V[m][j];
+                }
+            }
+        }
+    };
+
+    const train = () => {
+        forwardPropagation();
+        outputLayerDelta();
+        hiddenLayerDelta();
+        // updateWeights();
+        // console.log('Updated weights:', w);
+    };
     return (
         <div className='fifty'>
             <button type="button" onClick={forwardPropagation}>Alefaso</button>
+            <button type="button" onClick={train}>Jereo</button>
             <h1>Test</h1>
         </div>
     );
